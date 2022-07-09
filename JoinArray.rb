46f706493337b6a5eb7559b7a29fa201af0c0484
msbAtGit <bharathsound@gmail.com>
2022-07-09 07:10:32 +0530

    Prog Exercise: Wd 201 join arrays adding comments

--- JoinArray.rb
+++ JoinArray.rb
@@ -1,4 +1,5 @@
 names = [["Jhumpa", "Lahiri"], ["J. K", "Rowling"], ["Devdutt", "Pattanaik"]]
+#Joining the sub array with whitesapce to create <FIRSTNAME> SPACE <LASTNAME>
 full_names=names.map{|a| a.join(" ")}
 puts full_names
 
