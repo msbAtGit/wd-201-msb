85ef27b7f9c4c57319574a3b545b8be47a91cc67
msbAtGit <bharathsound@gmail.com>
2022-07-10 06:40:54 +0530

    WD201 L2 Functions: Greetings using last name and msg

---
+++ GreetingsFunction.rb
@@ -0,0 +1,7 @@
+def salute(fullName, msg)
+    lastName = fullName.split(" ").last.capitalize
+    greetingMsg = "#{msg.capitalize} Mr. #{lastName}"
+end
+
+puts salute("Nelson Rolihlahla Mandela", "hello")
+puts salute("Sir Alan Turing", "welcome")
